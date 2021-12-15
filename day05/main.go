a860b191b56f3477ddbe5c6b8f2b4c67eccfdc89
Pablo Acuna <pacuna@pm.me>
2021-12-14 20:16:58 -0600

    add day05 partII

--- day05/main.go
+++ day05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
@@ -66,6 +67,84 @@ func SolveI() {
 	fmt.Println(res)
 }
 
+func SolveII() {
+	f, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(f)
+
+	var (
+		x1 int
+		x2 int
+		y1 int
+		y2 int
+	)
+
+	covered := make(map[Point]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		r := strings.NewReader(line)
+		fmt.Fscanf(r, "%d,%d -> %d,%d", &x1, &y1, &x2, &y2)
+		p1 := Point{x: x1, y: y1}
+		p2 := Point{x: x2, y: y2}
+
+		var start, end int
+		if p1.x == p2.x {
+			if p1.y >= p2.y {
+				start = p2.y
+				end = p1.y
+			} else {
+				start = p1.y
+				end = p2.y
+			}
+			for i := start; i <= end; i++ {
+				covered[Point{x: p1.x, y: i}]++
+			}
+		} else if p1.y == p2.y {
+			if p1.x >= p2.x {
+				start = p2.x
+				end = p1.x
+			} else {
+				start = p1.x
+				end = p2.x
+			}
+			for i := start; i <= end; i++ {
+				covered[Point{x: i, y: p1.y}]++
+			}
+		} else if math.Abs(float64(p1.x-p2.x)) == math.Abs(float64(p1.y-p2.y)) {
+			if p2.x > p1.x {
+				if p2.y < p1.y {
+					for i := 0; i <= int(math.Abs(float64(p1.x-p2.x))); i++ {
+						covered[Point{x: p1.x + i, y: p1.y - i}]++
+					}
+				} else {
+					for i := 0; i <= int(math.Abs(float64(p1.x-p2.x))); i++ {
+						covered[Point{x: p1.x + i, y: p1.y + i}]++
+					}
+				}
+			} else {
+				if p2.y < p1.y {
+					for i := 0; i <= int(math.Abs(float64(p1.x-p2.x))); i++ {
+						covered[Point{x: p1.x - i, y: p1.y - i}]++
+					}
+				} else {
+					for i := 0; i <= int(math.Abs(float64(p1.x-p2.x))); i++ {
+						covered[Point{x: p1.x - i, y: p1.y + i}]++
+					}
+				}
+
+			}
+		}
+	}
+
+	res := 0
+	for _, v := range covered {
+		if v >= 2 {
+			res++
+		}
+	}
+	fmt.Println(res)
+}
+
 func main() {
-	SolveI()
+	SolveII()
 }
