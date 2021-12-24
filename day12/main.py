a144b273269d84a175f296d9e92e6b878bf61f5d
Pablo Acuna <pacuna@pm.me>
2021-12-23 20:37:10 -0600

    add day12 part II

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
-from typing import Any, Dict, List
+from collections import defaultdict, Counter
+from typing import Dict, List
 
 
 def build_adj_list(filename: str) -> Dict[str, List]:
@@ -7,30 +7,59 @@ def build_adj_list(filename: str) -> Dict[str, List]:
     for line in open(filename):
         n1, n2 = line.strip().split('-')
         graph[n1].append(n2)
-        if n1 != 'start':
-            graph[n2].append(n1)
+        graph[n2].append(n1)
 
     return graph
 
 
-def find_all_paths(graph, start, end, path=None):
-    if path is None:
-        path = []
-    path = path + [start]
-    if start == end:
-        return [path]
-    if start not in graph:
-        return []
+# Part I
+def find_all_paths(graph):
+    stack = [['start']]
     paths = []
-    for node in graph[start]:
-        if (node.islower() and path.count(node) == 0) or (node.isupper()):
-            new_paths = find_all_paths(graph, node, end, path)
-            for new_path in new_paths:
-                paths.append(new_path)
+
+    while stack:
+        path = stack.pop()
+        node = path[-1]
+        if node != 'end':
+            for neighbor in graph[node]:
+                print(neighbor)
+                if (neighbor.islower() and neighbor not in path) or (neighbor.isupper()):
+                    stack.append(path + [neighbor])
+        else:
+            paths += [path]
+    return paths
+
+# Part II
+def contains_dup(path: List[str]):
+    c = Counter(path)
+    for k, v in c.items():
+        if k.islower() and v > 1:
+            return True
+    return False
+
+
+def find_all_paths2(graph):
+    stack = [['start']]
+    paths = []
+
+    while stack:
+        path = stack.pop()
+        node = path[-1]
+        if node != 'end':
+            for neighbor in graph[node]:
+                if neighbor == 'start':
+                    continue
+                if (neighbor in path) and not contains_dup(path):
+                    stack.append(path + [neighbor])
+                elif (neighbor.islower() and neighbor not in path) or (neighbor.isupper()):
+                    stack.append(path + [neighbor])
+        else:
+            paths += [path]
     return paths
 
 
 if __name__ == '__main__':
     g = build_adj_list("data.in")
-    all_paths = find_all_paths(g, 'start', 'end')
-    print(len(all_paths))
+    p = find_all_paths2(g)
+    for idx, path in enumerate(p):
+        print(idx, path)
