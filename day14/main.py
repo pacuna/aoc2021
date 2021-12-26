7eee8c4d6d8938b389b4cad74ed2bbd9d2916da8
Pablo Acuna <pacuna@pm.me>
2021-12-26 17:40:12 -0600

    add part II

--- day14/main.py
+++ day14/main.py
@@ -21,8 +21,31 @@ for i in range(10):
         else:
             new_template += template[i:i+2]
     template = new_template
-
-c = collections.Counter(template)
-print(c)
+    c = collections.Counter(template)
+    print(c)
 
 # Part II
+
+pair_freq = collections.defaultdict(int)
+char_freq = collections.defaultdict(int)
+
+for i in range(len(template)-1):
+    pair_freq[template[i:i+2]] += 1
+
+for i in range(len(template)):
+    char_freq[template[i]] += 1
+
+tmp = pair_freq.copy()
+for i in range(40):
+    for pair, cnt in pair_freq.items():
+        if pair in rules:
+            tmp[pair[0]+rules[pair]] += cnt
+            tmp[rules[pair]+pair[1]] += cnt
+            char_freq[rules[pair]] += cnt
+            tmp[pair] -= cnt
+    pair_freq = tmp.copy()
+
+print(char_freq)
+
+sorted_vals = sorted(char_freq.values())
+print(sorted_vals[-1] - sorted_vals[0])
